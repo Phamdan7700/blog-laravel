cd9b2dd2324f33c2fd389509b01afe7f8f350fd6
Pham Dan <phamdan7700@gmail.com>
2021-03-07 16:37:12 +0700

    create Models

---
+++ database/factories/TypeOfNewsFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TypeOfNews;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TypeOfNewsFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = TypeOfNews::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
